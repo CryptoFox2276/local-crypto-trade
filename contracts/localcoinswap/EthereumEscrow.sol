3fac52a1ce6766892a5d73144fb96a0e71c572c2
JSTop1Coder <jstop1coder@gmail.com>
2023-06-19 06:31:57 +0900

    update compile version

--- contracts/localcoinswap/EthereumEscrow.sol
+++ contracts/localcoinswap/EthereumEscrow.sol
@@ -1,4 +1,5 @@
-pragma solidity ^0.5.17;
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.4;
 
 import "@openzeppelin/contracts/token/ERC20/SafeERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
